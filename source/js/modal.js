25c30875ba34f35c56521fb02622dec794f0259d
Egrik <egor-office-mail@yandex.ru>
2020-04-05 08:33:00 +0300

    Полночтью сделан 3, 4 и 5 экраны

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,23 +1,49 @@
-const btnMobileElement = document.querySelector(`.about-product__toggle`);
-const btmModalClose = document.querySelector(`.modal__close`);
+const btnForm = document.querySelectorAll(`.btn`);
 const formElement = document.querySelector(`.about-product__form`);
+const tabletFormElement = document.querySelector(`.modal-form`);
+const btmModalClose = document.querySelectorAll(`.modal__close`);
 const overlayElement = document.querySelector(`.overlay`);
+const ESC_KEYCODE = 27;
+const TABLET_WIDTH_WINDOW = 768;
 
-btnMobileElement.addEventListener(`click`, () => {
-  formElement.classList.add(`modal__show`);
-  overlayElement.classList.add(`overlay__show`)
-});
-
-btmModalClose.addEventListener(`click`, () => {
-  closeModal();
+btmModalClose.forEach((btnClose) => {
+  btnClose.addEventListener(`click`, () => {
+    closeModal();
+  })
 })
 
 overlayElement.addEventListener(`click`, () => {
   closeModal();
 })
 
+btnForm.forEach((button) => {
+  if (button.dataset.btn === `form`) {
+    button.addEventListener(`click`, () => {
+      if (clientWidth < TABLET_WIDTH_WINDOW) {
+        formElement.classList.add(`modal__show`);
+      } else {
+        tabletFormElement.classList.add(`modal__show`);
+      }
+
+      overlayElement.classList.add(`overlay__show`);
+      document.addEventListener(`keydown`, setEscKeydown);
+    })
+  }
+})
+
+const setEscKeydown = (evt) => {
+  if (evt.keyCode === ESC_KEYCODE) {
+    document.removeEventListener(`keydown`, setEscKeydown)
+    closeModal();
+  }
+}
 
 const closeModal = () => {
-  formElement.classList.remove(`modal__show`);
+  if (clientWidth < TABLET_WIDTH_WINDOW) {
+    formElement.classList.remove(`modal__show`);
+  } else {
+    tabletFormElement.classList.remove(`modal__show`);
+  }
+
   overlayElement.classList.remove(`overlay__show`);
 };
